83a8a75c5993f995c1eb33a8665626069aeb8d22
vukse95 <stefan26642@hotmail.com>
2017-06-13 11:35:09 +0200

    bilinearInterpolate function implemented, has some color loss! #2

--- src/ImageInterpolation.cpp
+++ src/ImageInterpolation.cpp
@@ -5,8 +5,8 @@
 
 void sampleAndHold(const uchar input[], int xSize, int ySize, uchar output[], int newXSize, int newYSize)
 {
-	// allocate
 
+	// allocate
 	uchar* Y_old = new uchar[xSize * ySize]();
 	char* U_old = new char[xSize * ySize / 4]();
 	char* V_old = new char[xSize * ySize / 4]();
@@ -26,8 +26,11 @@ void sampleAndHold(const uchar input[], int xSize, int ySize, uchar output[], in
 	int m = 0;
 	int k = 0;
 
-	for (int i = 0; i < newYSize; i++) {
-		for (int j = 0; j < newXSize; j++) {
+	// Y component
+	for (int i = 0; i < newYSize; i++) 
+	{
+		for (int j = 0; j < newXSize; j++) 
+		{
 
 			m = (i - 1) / s_vertical;
 			k = (j - 1) / s_horizontal;
@@ -43,8 +46,10 @@ void sampleAndHold(const uchar input[], int xSize, int ySize, uchar output[], in
 	}
 
 	// for U and V components 
-	for (int i = 0; i < newYSize / 2; i++) {
-		for (int j = 0; j < newXSize / 2; j++) {
+	for (int i = 0; i < newYSize / 2; i++) 
+	{
+		for (int j = 0; j < newXSize / 2; j++) 
+		{
 
 			m = (i - 1) / s_vertical;
 			k = (j - 1) / s_horizontal;
@@ -75,7 +80,101 @@ void sampleAndHold(const uchar input[], int xSize, int ySize, uchar output[], in
 
 void bilinearInterpolate(const uchar input[], int xSize, int ySize, uchar output[], int newXSize, int newYSize)
 {
-	/* TO DO */
+
+	// allocate
+	uchar* Y_old = new uchar[xSize * ySize]();
+	char* U_old = new char[xSize * ySize / 4]();
+	char* V_old = new char[xSize * ySize / 4]();
+
+	uchar* Y_new = new uchar[newXSize * newYSize]();
+	char* U_new = new char[newXSize * newYSize / 4]();
+	char* V_new = new char[newXSize * newYSize / 4]();
+
+	// convert from RGB to YUV
+	RGBtoYUV420(input, xSize, ySize, Y_old, U_old, V_old);
+
+	// scale
+	const double s_horizontal = (double)newXSize / xSize;
+	const double s_vertical = (double)newYSize / ySize;
+
+	int m = 0;
+	int k = 0;
+
+	int f = 0;
+	int d = 0;
+
+	for (int i = 0; i < newYSize; i++) 
+	{
+		for (int j = 0; j < newXSize; j++) 
+		{
+			double a = i / s_vertical - floor(i / s_vertical);
+			double b = j / s_horizontal - floor(j / s_horizontal);
+
+			m = i / s_vertical;
+			k = j / s_horizontal;
+
+			f = m;
+			d = k;
+
+			//out of range protection
+			if (m < ySize - 1)
+				f = m + 1;
+			if (k < xSize - 1)
+				d = k + 1;
+
+			Y_new[i * newXSize + j] =
+				(1 - a) * (1 - b) * Y_old[m * xSize + k] +
+				(1 - a) * b * Y_old[m * xSize + d] +
+				a * (1 - b) * Y_old[f * xSize + k] +
+				a * b * Y_old[f * xSize + d];
+
+		}
+	}
+
+	for (int i = 0; i < newYSize / 2; i++) 
+	{
+
+		for (int j = 0; j < newXSize / 2; j++) 
+		{
+			double a = i / s_vertical - floor(i / s_vertical);
+			double b = j / s_horizontal - floor(j / s_horizontal);
+
+			m = i / s_vertical;
+			k = j / s_horizontal;
+
+			f = m;
+			d = k;
+
+			//out of range protection
+			if (m < ySize / 2 - 1)
+				f = m + 1;
+			if (k < xSize / 2 - 1)
+				d = k + 1;
+
+			U_new[i * newXSize / 2 + j] =
+				(1 - a) * (1 - b) * U_old[m * xSize / 2 + k] +
+				(1 - a) * b * U_old[m * xSize / 2 + d] +
+				a * (1 - b) * U_old[f * xSize / 2 + k] +
+				a * b * U_old[f * xSize / 2 + d];
+
+			V_new[i * newXSize / 2 + j] =
+				(1 - a) * (1 - b) * V_new[m * xSize / 2 + k] +
+				(1 - a) * b * V_new[m * xSize / 2 + d] +
+				a * (1 - b) * V_new[f * xSize / 2 + k] +
+				a * b * V_new[f * xSize / 2 + d];
+		}
+	}
+
+	// revert from YUV to RGB
+	YUV420toRGB(Y_new, U_new, V_new, newXSize, newYSize, output);
+
+	delete[] Y_old;
+	delete[] U_old;
+	delete[] V_old;
+
+	delete[] Y_new;
+	delete[] U_new;
+	delete[] V_new;
 }
 
 void bicubicInterpolate(uchar input[], int xSize, int ySize, uchar output[], int newXSize, int newYSize)
